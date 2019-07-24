05a6a4b0d624684359cdcb3b1109a0d0c9701651
Ajdin Mujezinović <ajdin.m@gmail.com>
2019-07-24 13:47:05 +0200

    feature: Get candles until ts
    * Get candles until ts
    
    * update example
    
    * fix params
    
    * fix imports
    
    * fix imports
    
    * fix imports

--- ds-example.py
+++ ds-example.py
@@ -5,3 +5,5 @@ from passwords import dtn_product_id, dtn_login, dtn_password
 api = IQFeedApiWrapper((dtn_product_id, dtn_login, dtn_password))
 df = api.get_last_n_bars(symbol="XG#", freq_in_sec=60, n=10)
 print(df)
+df = api.get_candles_iqfeed(symbol="XG#", freq_in_sec=60, last_ts=1563965400)
+print(df)
--- pyiqfeed/iqfeed_api_wrapper.py
+++ pyiqfeed/iqfeed_api_wrapper.py
@@ -6,23 +6,27 @@ from .exceptions import NoDataError, UnexpectedField, UnexpectedMessage
 from .exceptions import UnexpectedProtocol, UnauthorizedError
 
 
+import datetime as dt
+import pytz
+
+
 class IQFeedApiWrapper:
     def __init__(self, credentials):
         self.credentials = credentials
         self.launch_service()
 
     def launch_service(self):
-        """Check if IQFeed.exe is running and start if not"""         
+        """Check if IQFeed.exe is running and start if not"""
         product, login, password = self.credentials
         svc = FeedService(product=product,
-                            version="Debugging",
-                            login=login,
-                            password=password)
+                          version="Debugging",
+                          login=login,
+                          password=password)
         svc.launch(headless=False)
 
-
     def get_last_n_bars(self, symbol: str, freq_in_sec: int, n: int, bar_unit: str = 's'):
-        hist_conn = HistoryConn(name="dynamicasoft-historical-bars-for-trading")
+        hist_conn = HistoryConn(
+            name="dynamicasoft-historical-bars-for-trading")
         hist_listener = VerboseIQFeedListener("History Bar Listener")
         hist_conn.add_listener(hist_listener)
 
@@ -31,10 +35,28 @@ class IQFeedApiWrapper:
             # request_bars_in_period for other ways to specify time periods etc
             try:
                 bars = hist_conn.request_bars(ticker=symbol,
-                                            interval_len=freq_in_sec,
-                                            interval_type=bar_unit,
-                                            max_bars=n)
-                return bars
+                                              interval_len=freq_in_sec,
+                                              interval_type=bar_unit,
+                                              max_bars=n)
+                return bars.iloc[1:]
             except (NoDataError, UnauthorizedError) as err:
                 print("No data returned because {0}".format(err))
                 return False
+
+    def get_candles_iqfeed(self, symbol: str, last_ts: int, freq_in_sec: int):
+        now_ts = int(pytz.UTC.localize(
+            dt.datetime.now(), is_dst=True).timestamp())
+        expected_bars = int(((now_ts - last_ts) / freq_in_sec) + 0.5)
+        bars = self.get_last_n_bars(symbol, freq_in_sec, expected_bars)
+        if bars is False:
+            return False
+
+        def to_ts(datetime_str):
+            tz = pytz.timezone('America/New_York')
+            time = dt.datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:00')
+            time = tz.localize(time, is_dst=True)
+            time = time.astimezone(pytz.utc)
+            return int(time.timestamp())
+
+        bars["tz-time"] = bars["datetime"].map(to_ts)
+        return bars[bars["tz-time"] > last_ts]
